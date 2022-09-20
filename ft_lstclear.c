1858d17f422c78801d092f72cbb3523374b3e615
Larrain <larraingonzalez@gmail.com>
2022-09-20 18:21:40 +0200

    libft finished except lstmap

---
+++ ft_lstclear.c
@@ -0,0 +1,15 @@
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*new;
+
+	while (lst)
+	{
+		new = (*lst).next;
+		ft_lstdelone(*lst, del);
+		*lst = new;
+	}
+	lst = NULL;
+}
